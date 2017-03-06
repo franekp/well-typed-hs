2b3e55688763a7ca8db2f6503c22c55f7759bdfd
Franciszek Piszcz <franek@qed.ai>
2017-03-06 17:41:33 +0100

    Rename make_quantifiers_common -> synchronize_quantifiers.

--- Unification.hs
+++ Unification.hs
@@ -20,18 +20,18 @@ import Data.Typeable (Typeable, cast)
 import Types
 import Instances hiding (main)
 
-make_quantifiers_common :: Poly t -> Poly t -> (Poly t, Poly t)
-make_quantifiers_common (MonoP a) (MonoP b) = (MonoP a, MonoP b)
-make_quantifiers_common (MonoP a) b = (aa, bb) where
-  (bb, aa) = make_quantifiers_common b (MonoP a)
-make_quantifiers_common (ForallP id_a a_) b_ =
+synchronize_quantifiers :: Poly t -> Poly t -> (Poly t, Poly t)
+synchronize_quantifiers (MonoP a) (MonoP b) = (MonoP a, MonoP b)
+synchronize_quantifiers (MonoP a) b = (aa, bb) where
+  (bb, aa) = synchronize_quantifiers b (MonoP a)
+synchronize_quantifiers (ForallP id_a a_) b_ =
   let
     helper_left :: Any a => ExistsPoly t a -> ExistsPoly t a -> ExistsPoly t a
     helper_left (ExistsPoly a_) (ExistsPoly b_) =
-      let (aa, bb) = make_quantifiers_common a_ b_ in ExistsPoly aa
+      let (aa, bb) = synchronize_quantifiers a_ b_ in ExistsPoly aa
     helper_right :: Any a => ExistsPoly t a -> ExistsPoly t a -> ExistsPoly t a
     helper_right (ExistsPoly a_) (ExistsPoly b_) =
-      let (aa, bb) = make_quantifiers_common a_ b_ in ExistsPoly bb
+      let (aa, bb) = synchronize_quantifiers a_ b_ in ExistsPoly bb
   in
   (ForallP id_a $ helper_left a_ (ExistsPoly b_), ForallP id_a $ helper_right a_ (ExistsPoly b_))
 
@@ -97,7 +97,7 @@ unpack_poly a b =
       (ForallP num_a (ExistsPoly poly_a :: ExistsPoly t a))
       (ForallP num_b (ExistsPoly poly_b :: ExistsPoly t a ))
       (VarMapping m) =
-        if num_a /= num_b then error "make_quantifiers_common not called before unpack_poly!" else
+        if num_a /= num_b then error "synchronize_quantifiers not called before unpack_poly!" else
         unpack_poly' (SuccTV last_tv) poly_a poly_b $ VarMapping $ (num_a, Mono last_tv):m
 
 data Constraint = Constraint (Mono TypeVar) (Mono Type)
@@ -164,12 +164,12 @@ unify :: forall t u. (forall a. Any a => t a -> Type a) -> Poly t
   -> (forall a. Any a => t a -> Type a) -> Poly t
   -> (forall a b. t a -> t b -> Poly u) -> Poly u
 unify f_a a_input f_b b_input cont =
-  let (a_poly, b_poly) = make_quantifiers_common a_input b_input in
+  let (a_poly, b_poly) = synchronize_quantifiers a_input b_input in
   let (a_mono, b_mono, m, start_var) = unpack_poly a_poly b_poly in
   undefined
 
 main = do
-  let (e1, e2) = make_quantifiers_common types_example_1 types_example_2
+  let (e1, e2) = synchronize_quantifiers types_example_1 types_example_2
   putStrLn $ show $ e1
   putStrLn $ show $ e2
   putStrLn $ show $ unpack_poly e1 e2
