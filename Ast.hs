f08c8ea0aa9acfc32dd2584192cb5b3e0be49c1d
Franciszek Piszcz <franek@qed.ai>
2017-03-12 12:32:02 +0100

    Code style.

--- Ast.hs
+++ Ast.hs
@@ -25,37 +25,36 @@ import Instances hiding (main)
 import Unification
 
 data Store :: * -> * where
-  NilS :: Store Nil
-  ConsS :: (A Type h, Typeable t) => h -> Store t -> Store (Cons h t)
+  NilS ::
+    Store Nil
+  ConsS :: (A Type h, Typeable t) =>
+    h -> Store t -> Store (Cons h t)
 
 deriving instance Typeable1 Store
 
 data Env :: * -> * where
-  NilEN :: Env Nil
-  ConsEN :: (A Type h, Typeable t) => String -> Type h -> Env t -> Env (Cons h t)
-  LetEN :: String -> Poly (Ast e) -> Env e -> Env e
+  NilEN ::
+    Env Nil
+  ConsEN :: (A Type h, Typeable t) =>
+    String -> Type h -> Env t -> Env (Cons h t)
+  LetEN ::
+    String -> Poly (Ast e) -> Env e -> Env e
 
 deriving instance Typeable1 Env
 
 data Ast :: * -> * -> * where
   AddA :: Typeable e =>
     Ast e (Int -> Int -> Int)
-
   LiteralA :: Typeable e =>
     Int -> Ast e Int
-
   VarA :: (A Type a, Typeable e) =>
     Ast (Cons a e) a
-
   LiftA :: (A Type a, A Type b, Typeable e) =>
     Ast e a -> Ast (Cons b e) a
-
   LambdaA :: (A Type a, A Type b, Typeable e) =>
     Ast (Cons a e) b -> Ast e (a -> b)
-
   ErrorA :: (A Type a, Typeable e) =>
     String -> Ast e a
-
   AppA :: (A Type a, A Type b, Typeable e) =>
     Ast e (a -> b) -> Ast e a -> Ast e b
 
--- Types.hs
+++ Types.hs
@@ -31,15 +31,22 @@ data TypeHole
   deriving Typeable
 
 data Type :: * -> * where
-  ArrowTT :: (A Type a, A Type b) => Type a -> Type b -> Type (a -> b)
-  IntTT :: Type Int
-  VoidTT :: Type Void
-  TypeVarTT :: A TypeVar a => TypeVar a -> Type a
-  TypeHoleTT :: Type TypeHole
+  ArrowTT :: (A Type a, A Type b) =>
+    Type a -> Type b -> Type (a -> b)
+  IntTT ::
+    Type Int
+  VoidTT ::
+    Type Void
+  TypeVarTT :: A TypeVar a =>
+    TypeVar a -> Type a
+  TypeHoleTT ::
+    Type TypeHole
 
 data TypeVar :: * -> * where
-  ZeroTV :: TypeVar Zero
-  SuccTV :: A TypeVar a => TypeVar a -> TypeVar (Succ a)
+  ZeroTV ::
+    TypeVar Zero
+  SuccTV :: A TypeVar a =>
+    TypeVar a -> TypeVar (Succ a)
 
 class (Typeable a, Typeable1 t) => A t a where
   anything :: t a
