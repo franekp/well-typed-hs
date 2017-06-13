bcf5b32dac1e54e97d17b8267834a581086a044d
Franciszek Piszcz <franek@qed.ai>
2017-06-13 13:36:33 +0200

    Add a switch to turn type inference on/off

--- Main.hs
+++ Main.hs
@@ -21,11 +21,11 @@ type ParseFun a = [Token] -> Err a
 
 myLLexer = resolveLayout True . myLexer
 
-runFile :: ParseFun (Pos Module) -> FilePath -> IO ()
-runFile p f = {-putStrLn f >>-} readFile f >>= run p f
+runFile :: Bool -> ParseFun (Pos Module) -> FilePath -> IO ()
+runFile ti p f = {-putStrLn f >>-} readFile f >>= run ti p f
 
-run :: ParseFun (Pos Module) -> String -> String -> IO ()
-run p filename s = let ts = myLLexer s in case p ts of
+run :: Bool -> ParseFun (Pos Module) -> String -> String -> IO ()
+run ti p filename s = let ts = myLLexer s in case p ts of
   Bad s -> do
     putStrLn "\nParse              Failed...\n"
     putStrLn "Tokens:"
@@ -36,7 +36,7 @@ run p filename s = let ts = myLLexer s in case p ts of
     let uast = transModule (filename, s) tree
     module_env <- load_ext_modules uast
     let module_type_env = get_ext_module_types module_env
-    let uast' = run_algorithm_w module_type_env uast
+    let uast' = if ti then run_algorithm_w module_type_env uast else uast
     uast_to_io module_env uast'
     --putStrLn $ uast_to_string module_env uast
 
@@ -46,5 +46,5 @@ main = do
   case args of
     [] -> putStrLn "No input files."
     --[] -> hGetContents stdin >>= run pModule
-    "-s":fs -> mapM_ (runFile pModule) fs
-    fs -> mapM_ (runFile pModule) fs
+    "--no-inference":fs -> mapM_ (runFile False pModule) fs
+    fs -> mapM_ (runFile True pModule) fs
